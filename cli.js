2d8c02c4d46516a63c3d706cd232617c3ca6227b
coderanac <coder.anac@gmail.com>
2020-07-05 04:10:08 -0300

    :recycle: refac cli

--- cli.js
+++ cli.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 
-const { response, responseStats } = require('./scripts/response');
+const { getLinks, getLinksWithValidation } = require('./main');
+const { renderLog, simpleLogger, statsLogger, renderLogWithStats, statsWithValidateLogger, validateLogger } = require('./loggers');
 
 console.log('Processing file 🧶🐱');
 
+const path = process.argv[2];
+
 switch (true) {
   case process.argv.includes('--validate') && process.argv.includes('--stats'):
-    responseStats(true);
+    getLinksWithValidation(path).then((links) => renderLogWithStats(links, statsWithValidateLogger));
     break;
   case process.argv.includes('--validate'):
-    response(true);
+    getLinksWithValidation(path).then((links) => renderLog(links, validateLogger));
     break;
   case process.argv.includes('--stats'):
-    responseStats(false);
+    renderLogWithStats(getLinks(path), statsLogger);
     break;
   default:
-    response(false);
+    renderLog(getLinks(path), simpleLogger);
     break;
 }
